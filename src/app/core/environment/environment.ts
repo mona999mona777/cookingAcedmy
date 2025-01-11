32ca3c89a58b4d8f361c4e6fddbf6c01cc2148d6
mariamyasser <maruamyasser@mariam yasser>
2025-01-11 11:54:32 +0200

    acedmycookingapping

--- src/app/core/environment/environment.ts
+++ src/app/core/environment/environment.ts
@@ -1,7 +1,6 @@
 export const environment={
     baseUrl:"https://ecommerce.routemisr.com",
     secBaseUrl:"https://www.themealdb.com/api/json/v1/1/",
-    urlServer:"https://mariamyasser-cookingacedmyapps.netlify.app"
-
+    urlServer:"https://mariamyasser-cookingacedmy-ready-app.netlify.app"
     }
 
